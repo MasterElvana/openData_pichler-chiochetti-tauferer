905b071f97ff2dc4146a081350040e44042baa97
MasterElvana <90690602+MasterElvana@users.noreply.github.com>
2022-01-27 12:21:30 +0100

    access to service

--- charge/src/app/app.module.ts
+++ charge/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CurrentstationComponent } from './currentstation/currentstation.component';
+import { CurrentplugsComponent } from './currentplugs/currentplugs.component';
+import { AccessComponent } from './access/access.component';
+import { NumberComponent } from './number/number.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentstationComponent,
+    CurrentplugsComponent,
+    AccessComponent,
+    NumberComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ charge/src/app/currentstation/currentstation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tp-currentstation',
+  templateUrl: './currentstation.component.html',
+  styleUrls: ['./currentstation.component.scss']
+})
+export class CurrentstationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
