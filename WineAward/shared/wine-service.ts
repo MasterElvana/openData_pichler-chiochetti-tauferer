00ff605d3a596e3df033b8678b19705e22e05fb1
MasterElvana <90690602+MasterElvana@users.noreply.github.com>
2022-02-02 16:08:07 +0100

    wine

---
+++ WineAward/shared/wine-service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient, HttpErrorResponse, HttpResponse, HttpResponseBase } from '@angular/common/http';
+
+const URL = 'https://api.tourism.testingmachine.eu/v1/WineAward';
+
+@Injectable()
+export class WineService {
+ wine: Wine | null = null;
+ error: HttpErrorResponse | null = null;
+ response: HttpResponse<Wine> | null = null;
+
+ constructor(private http: HttpClient) { }
+
+ getWine(titel: string) {
+  return this.http.get<Wine>(`${URL}/wine/${titel}`);
+ }
+
+ getAllWineSearchTerm(searchTerm: string): Observable<Wine[]> {
+ return this.http
+ .get<Wine[]>(`${URL}/wines/search/${searchTerm}`);
+ }
+
+ getAllWines(){
+  return this.http
+  .get<Wine[]>(`${URL}/wines`);
+ };
+
+}
